5be481acfab8c8667870fb89122daed13cf758ca
Sopnil Swapon <sopnilswapon1@gmail.com>
2024-04-27 08:42:44 +0600

    added get singel spot by id from database system

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
 
@@ -42,6 +42,12 @@ async function run() {
       const spots = req.body;
       const cursor = tourSpotsCollections.find(spots);
       const result = await cursor.toArray();
+      res.send(result)
+    })
+    app.get('/spots/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await tourSpotsCollections.findOne(query);
       res.send(result)
     })
     // Send a ping to confirm a successful connection
