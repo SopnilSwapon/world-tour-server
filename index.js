d7187040c0dee05f8400f5cd1acdd5d7168c7b23
Sopnil Swapon <sopnilswapon1@gmail.com>
2024-04-28 13:28:28 +0600

    added deleted method to delete a singel spot from list

--- index.js
+++ index.js
@@ -66,6 +66,12 @@ async function run() {
       };
       const result = await tourSpotsCollections.updateOne(query, updateSpot, options)
       res.send(result)
+  });
+  app.delete('/spots/:id', async (req, res) => {
+    const id = req.params.id;
+    const query = { _id: new ObjectId(id)};
+    const result = await tourSpotsCollections.deleteOne(query);
+    res.send(result)
   })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
